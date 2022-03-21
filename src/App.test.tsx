b6a0f79df4dcb0ed33c695c4caf084162f01c5f1
szamanr <szamanr@protonmail.com>
2022-03-21 16:52:11 +0100

    add prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 import { MockedProvider, MockedResponse } from "@apollo/client/testing";
 
 const mocks: MockedResponse[] = [];
 
-test('renders learn react link', () => {
-  render(<MockedProvider mocks={mocks} addTypename={false}><App/></MockedProvider>);
+test("renders learn react link", () => {
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <App />
+    </MockedProvider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: 'https://rickandmortyapi.com/graphql',
+  uri: "https://rickandmortyapi.com/graphql",
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App/>
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
