f8f7ef1e461c199d565a2f266d9aaa904295e1b1
szamanr <szamanr@protonmail.com>
2022-03-21 16:49:00 +0100

    add apollo client

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+
+const mocks: MockedResponse[] = [];
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<MockedProvider mocks={mocks} addTypename={false}><App/></MockedProvider>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
